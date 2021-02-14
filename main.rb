b4a91d645e8894488f0773ef19b99119fe271e98
nick ater <nickolas.ater@gmail.com>
2021-02-14 16:49:45 -0500

    ran rubocop

--- main.rb
+++ main.rb
@@ -1,22 +1,20 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require_relative './vending-item';
-require_relative './vending-machine';
+require_relative './vending-item'
+require_relative './vending-machine'
 require 'csv'
 
-products = Array.new
+products = []
 
 CSV.foreach('./products.csv') do |row|
-    product = VendingItem.new(row[0], row[1], row[2])
-    products.push(product)
+  product = VendingItem.new(row[0], row[1], row[2])
+  products.push(product)
 end
 
-vendingMachine = VendingMachine.new(products);
+vendingMachine = VendingMachine.new(products)
 
 while vendingMachine.is_on
-    selection = vendingMachine.showMainMenu
-    vendingMachine.handleUserInput(selection)
+  selection = vendingMachine.showMainMenu
+  vendingMachine.handleUserInput(selection)
 end
-
-
-
--- vending-item.rb
+++ vending-item.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 class VendingItem
+  attr_accessor :selector, :name, :price, :quantity
 
-    attr_accessor :selector, :name, :price, :quantity;
-
-    def initialize(selector, name, price)
-        @selector = selector
-        @name = name
-        @price = price
-        @quantity = 5
-    end
-
+  def initialize(selector, name, price)
+    @selector = selector
+    @name = name
+    @price = price
+    @quantity = 5
+  end
 end
